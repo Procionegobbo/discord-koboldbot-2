a8d56d6e2d6e1dc07e3425fc8d45427a333586fe
Federico Maiorini <procionegobbo@gmail.com>
2021-04-21 18:12:40 +0200

    First Commit of v2

--- app.js
+++ app.js
@@ -1,41 +1,108 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// Extract the required classes from the discord.js module
+const { Client, MessageEmbed } = require('discord.js');
 
-var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
+// Create an instance of a Discord client
+//const client = new Client({autoReconnect: true});
+const client = new Client();
 
-var app = express();
+//const keepAlive = require('./server');
+const generateKobold = require('./koboldgrammar');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+const armaUrl = 'https://grammar.procionegobbo.it/grammar/arma-fantasy-json';
+const pngUrl = 'https://grammar.procionegobbo.it/grammar/png-json';
+const koboldUrl = 'https://grammar.procionegobbo.it/grammar/koboldoJson';
 
-app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+client.on('ready', () => {
+    console.log(`RandomKoboldBot è online ${client.user.tag}!`);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+client.on('message', msg => {
+    if (msg.content.toUpperCase().startsWith('KOB')) {
+        console.log('Comando ricevuto: ' + msg.content);
+        //console.log(msg);
+        if (msg.content.toUpperCase() === 'KOBKOBOLDO') {
+            console.log(msg.author.username + " ha chiesto un koboldo nel canale " + msg.channel.name);
+            msg.channel.send('Koboldo in arrivo!');
+            let arma = generateKobold('arma-fantasy-json');
+            let kobold = generateKobold('koboldoJson');
+            let png = generateKobold('png-json');
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+            //console.log(kobold);
+            //console.log(arma);
+            //console.log(png);
+            let responseText = "" +
+                //"**Nome**: " + kobold.Nomekoboldo + " " + kobold.CogNomekoboldo + "\n" +
+                "**Origine**: " + kobold.Originekoboldo + "\n" +
+                "**Scaglie**: " + kobold.Colorekoboldo + "\n" +
+                "**Segni particolari**: " + kobold.SegnoParticolare + "\n" +
+                "**Parentela con i coboldi di Fumble!**: " + kobold.ParentelaKoboldo + "\n" +
+                "**Ricetta preferita**: " + kobold.PiattoKoboldo + "\n" +
+                "**Arma preferita**: " + arma.arma + "\n" +
+                "**Occupazione attuale**: " + png.png + "\n" +
+                "**Storia**: " + kobold.StoriaKoboldo + "";
+            const embed = new MessageEmbed()
+                .setTitle(kobold.Nomekoboldo + " " + kobold.CogNomekoboldo)
+                .setColor(0xFF0000)
+                .setDescription(responseText);
+            msg.channel.send(embed)
+                .then(message => console.log('Sent #' + message.id + ': ' + message.content))
+                .catch(console.error);
+
+        }
+        if (msg.content.toUpperCase() === 'KOBOCCUPAZIONE') {
+            let png = generateKobold('png-json');
+            const embed = new MessageEmbed()
+                .setTitle('Occupazione da koboldo')
+                .setColor(0xFF0000)
+                .setDescription('*' + png.png + '*');
+            msg.channel.send(embed)
+                .then(message => console.log('Sent #' + message.id + ': ' + message.content))
+                .catch(console.error);
+
+        }
+        if (msg.content.toUpperCase() === 'KOBARMA') {
+            let arma = generateKobold('arma-fantasy-json');
+            const embed = new MessageEmbed()
+                .setTitle('Arma da koboldo')
+                .setColor(0xFF0000)
+                .setDescription(arma.arma);
+            msg.channel.send(embed)
+                .then(message => console.log('Sent #' + message.id + ': ' + message.content))
+                .catch(console.error);
+        }
+
+        if (msg.content.toUpperCase() === 'KOBRICETTA') {
+            let kobold = generateKobold('koboldoJson');
+            const embed = new MessageEmbed()
+                .setTitle('Ricetta kobolda')
+                .setColor(0xFF0000)
+                .setDescription(kobold.PiattoKoboldo);
+            msg.channel.send(embed)
+                .then(message => console.log('Sent #' + message.id + ': ' + message.content))
+                .catch(console.error);
+        }
+
+        if (msg.content.toUpperCase() === 'KOBAIUTO') {
+            let responseText = "Ciao sono il generatore casuale di koboldi." +
+                "Per estrarre qualcosa dal libro di **Krubal** usa questi comandi:\n" +
+                "**kobKoboldo**: per estrarre un koboldo casuale\n" +
+                "**kobArma**: per estrarre un'arma (spesso strana e inusuale)\n" +
+                "**kobOccupazione**: per trovare un'occupazione da koboldo (lavoro o passatempo)\n" +
+                "**kobRicetta**: per scoprire l'arte culinaria dei koboldi\n" +
+                "**kobAiuto**: per rivedere questo testo";
+            const embed = new MessageEmbed()
+                .setTitle('RandomKoboldBot')
+                .setColor(0xFF0000)
+                .setDescription(responseText);
+            msg.channel.send(embed)
+                .then(message => console.log('Sent #' + message.id + ': ' + message.content))
+                .catch(console.error);
+        }
+    }
 });
 
-module.exports = app;
+
+//keepAlive();
+
+client.login(process.env.DISCORD_TOKEN);
